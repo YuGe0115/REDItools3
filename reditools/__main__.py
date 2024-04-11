014a0073c5690aed3851b01a990d4a7dab54d82b
Adam Handen <adam.handen@gmail.com>
2024-04-11 10:26:38 -0500

    Alu index (#18)
    * Added editing indices calculations

--- reditools/__main__.py
+++ reditools/__main__.py
@@ -1,38 +1,37 @@
 """Commandline tool for REDItools."""
 
-import argparse
 import sys
 
-from reditools import analyze, homopolymerics
+from reditools import analyze, homopolymerics, index
 
 
 def usage():
     """Print program usage."""
-    parser = argparse.ArgumentParser(
-        description='REDItools 2.0',
-        prog='reditools',
-    )
-    parser.add_argument(
-        'command',
-        choices=[
-            'analyze',
-            'find-repeats',
-        ],
-        help='Either analyze or find-repeats.',
-    )
-    parser.parse_args()
+    print("""usage: reditools {analyze,find-repeats,index}
 
-    
+REDItools3
+
+Run Modes:
+  analyze            Find editing events in one or more alignment files.
+
+  find-repeats       Find repetitive elements in a genome.
+
+  index              Calculate editing indices from the output of `analyze`
+                     mode.
+""")
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        command = sys.argv[1]
-        if command == 'analyze':
-            sys.argv.pop(1)
-            analyze.main()
-        elif command == 'find-repeats':
-            sys.argv.pop(1)
-            homopolymerics.main()
-        else:
-            usage()
+        command = sys.argv.pop(1)
+        match command:
+            case 'analyze':
+                analyze.main()
+            case 'find-repeats':
+                homopolymerics.main()
+            case 'index':
+                index.main()
+            case _:
+                usage()
     else:
         usage()
